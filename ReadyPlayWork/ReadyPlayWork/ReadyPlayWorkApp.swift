1b643ae472a1985682fb874179fdfd39a479cc19
JeongHyunKim <kjhhaha@gmail.com>
2023-06-23 14:10:23 +0900

    StartProject files
    StartProject

---
+++ ReadyPlayWork/ReadyPlayWork/ReadyPlayWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadyPlayWorkApp.swift
+//  ReadyPlayWork
+//
+//  Created by Hyuny on 2023/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ReadyPlayWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
