bb896e72c4ac7f95cabd1d303846f29aa27e88e3
kjhhaha@gmail.com <JeongHyun1!>
2024-02-02 16:59:24 +0900

    Lint 적용
    -Swift Lint 적용.

---
+++ ReadyPlayWorkUIKit/ReadyPlayWorkUIKit/Scenes/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  ReadyPlayWorkUIKit
+//
+//  Created by JeongHyun Kim on 2/2/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ddd")
+    }
+
+}
